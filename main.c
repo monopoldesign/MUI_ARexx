eef46d9c5213bfa7c0a58f6e2264a38e8bcfa093
monopoldesign <mario.volkel@outlook.com>
2022-05-04 19:51:40 +0100

    First Sketch

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* MUI_ACT - MUI-ARexx/Commodity-Test
+* MUI_ARexx - ARexx-Test
 * (C)2022 M.Volkel (mario.volkel@outlook.com)
 *******************************************************************************/
 
@@ -36,12 +36,6 @@
 /******************************************************************************
 * Prototypes
 *******************************************************************************/
-HOOKPROTONH(HotKeyFunc, ULONG, Object *obj, CxMsg *cmsg);
-HOOKPROTONH(arexxFName, ULONG, Object *obj, char **msg);
-HOOKPROTONH(arexxDelay, ULONG, Object *obj, char **msg);
-HOOKPROTONH(ButtonFunc, ULONG, Object *obj, int *msg);
-HOOKPROTONH(MenuFunc, ULONG, Object *obj, int *msg);
-
 void init(void);
 void end(void);
 struct ObjApp *CreateApp(void);
@@ -52,185 +46,29 @@ void DisposeApp(struct ObjApp *ObjectApp);
 *******************************************************************************/
 #define MAKE_ID(a, b, c, d) ((ULONG)(a) << 24 | (ULONG)(b) << 16 | (ULONG)(c) << 8 | (ULONG)(d))
 
-#define ID_ARexx 1
-enum BUTID {ID_BT0, ID_BT1};
-enum MENUID {ID_MN_NEW, ID_MN_LOAD, ID_MN_CSON, ID_MN_CSOFF};
-
 struct ObjApp
 {
 	APTR	App;
 	APTR	WI_label_0;
-	APTR	MN_label_0;
-	APTR	MN_CS_ON, MN_CS_OFF;
-	APTR	TX_label_0;
-	APTR	BT_label_0;
-	APTR	BT_label_1;
-	char *	STR_TX_label_0;
+	APTR	TX_Receive;
+	APTR	STR_Value;
+	APTR	SL_Value2;
+	APTR	BT_Send;
+	char *	STR_TX_Receive;
 };
-
-#define CX_HOTKEY 1
 
 /******************************************************************************
 * Global Variables
 *******************************************************************************/
 struct IntuitionBase *IntuitionBase;
 struct Library *MUIMasterBase;
-struct Library *CxBase;
-
-MakeHook(hook_hotkey, HotKeyFunc);
-MakeHook(hook_fname, arexxFName);
-MakeHook(hook_delay, arexxDelay);
-MakeHook(hook_button, ButtonFunc);
-MakeHook(hook_menu, MenuFunc);
 
 char buffer[40];
 struct ObjApp *App = NULL;
 
-CxObj *broker, *filter, *sender, *translate;
-struct MsgPort *broker_mp;
-
-struct MUI_Command rexxCommands[] =
-{
-	{"test",	MC_TEMPLATE_ID,	ID_ARexx,	NULL},			// Simple ARexx-Command without Parameters
-	{"fname",	"NAME/A",		1,			&hook_fname},
-	{"delay",	"DELAY/A",		1,			&hook_delay},
-	{NULL,		NULL, 			NULL,		NULL}
-};
-
-BOOL clearScreen = TRUE;
 /******************************************************************************
 * Main-Program
 *******************************************************************************/
-
-/*-----------------------------------------------------------------------------
-- HotKeyFunc()
-- Function for Hotkey-Commodity-Hook
-------------------------------------------------------------------------------*/
-HOOKPROTONH(HotKeyFunc, ULONG, Object *obj, CxMsg *cmsg)
-{
-	ULONG cmsgid, cmsgtype;
-
-	cmsgid = CxMsgID(cmsg);
-	cmsgtype = CxMsgType(cmsg);
-
-	switch (cmsgtype)
-	{
-		case CXM_IEVENT:
-			switch (cmsgid)
-			{
-				case CX_HOTKEY:
-					printf("Hotkey detected\n");
-					break;
-			}
-			break;
-
-		case CXM_COMMAND:
-			switch (cmsgid)
-			{
-				case CXCMD_APPEAR:
-					printf("Show Interface\n");
-					break;
-				case CXCMD_DISAPPEAR:
-					printf("Hide Interface\n");
-					break;
-				case CXCMD_ENABLE:
-					printf("Enable Program\n");
-					break;
-				case CXCMD_DISABLE:
-					printf("Disable Program\n");
-					break;
-				case CXCMD_KILL:
-					printf("Kill Program\n");
-					break;
-				case CXCMD_UNIQUE:
-					printf("CXCMD_UNIQUE\n");
-					break;
-				default:
-					break;
-			}
-			break;
-	}
-
-	return 0;
-};
-
-/*-----------------------------------------------------------------------------
-- arexxFName()
-- Function for "fname"-ARexx-Hook
-------------------------------------------------------------------------------*/
-HOOKPROTONH(arexxFName, ULONG, Object *obj, char **msg)
-{
-	char *data = (char *)msg[0];
-
-	DisplayBeep(NULL);
-	printf("We got an incoming fname-command, value = %s\n", data);
-
-	return 0;
-}
-
-/*-----------------------------------------------------------------------------
-- arexxDelay()
-- Function for "delay"-ARexx-Hook
-------------------------------------------------------------------------------*/
-HOOKPROTONH(arexxDelay, ULONG, Object *obj, char **msg)
-{
-	unsigned int ndata = (unsigned int)atoi(msg[0]);
-
-	DisplayBeep(NULL);
-	printf("We got an incoming delay-command, value = %ld\n", ndata);
-
-	return 0;
-}
-
-/*-----------------------------------------------------------------------------
-- ButtonFunc()
-- Function for Button-Hook
-------------------------------------------------------------------------------*/
-HOOKPROTONH(ButtonFunc, ULONG, Object *obj, int *msg)
-{
-	switch (*msg)
-	{
-		case ID_BT0:
-			printf("Button 0\n");
-			break;
-		case ID_BT1:
-			printf("Button 1\n");
-			break;
-	}
-
-	return 0;
-}
-
-/*-----------------------------------------------------------------------------
-- MenuFunc()
-- Function for Menu-Hook
-------------------------------------------------------------------------------*/
-HOOKPROTONH(MenuFunc, ULONG, Object *obj, int *msg)
-{
-	switch (*msg)
-	{
-		case ID_MN_NEW:
-			printf("Menu: New\n");
-			break;
-		case ID_MN_LOAD:
-			printf("Menu: Load\n");
-			break;
-		case ID_MN_CSON:
-			clearScreen = TRUE;
-			set(App->MN_CS_ON, MUIA_Menuitem_Checked, TRUE);
-			set(App->MN_CS_OFF, MUIA_Menuitem_Checked, FALSE);
-			printf("ClearScreen On\n");
-			break;
-		case ID_MN_CSOFF:
-			clearScreen = FALSE;
-			set(App->MN_CS_ON, MUIA_Menuitem_Checked, FALSE);
-			set(App->MN_CS_OFF, MUIA_Menuitem_Checked, TRUE);
-			printf("ClearScreen Off\n");
-			break;
-	}
-
-	return 0;
-}
 
 /*-----------------------------------------------------------------------------
 - main()
@@ -250,21 +88,6 @@ int main(int argc, char *argv[])
 		end();
 	}
 
-	// Get Broker and Broker-MsgPort from MUI-App
-	get(App->App, MUIA_Application_Broker, (ULONG *)&broker);
-	get(App->App, MUIA_Application_BrokerPort, (ULONG *)&broker_mp);
-
-	// Initialize Hotkey
-	filter = CxFilter("rawkey rcommand l");
-	AttachCxObj(broker, filter);
-	sender = CxSender(broker_mp, CX_HOTKEY);
-	AttachCxObj(filter, sender);
-	translate = CxTranslate(NULL);
-	AttachCxObj(filter, translate);
-
-	// Activate Broker
-	ActivateCxObj(broker, 1L);
-
 	while (running)
 	{
 		switch (DoMethod(App->App, MUIM_Application_Input, &signal))
@@ -273,12 +96,6 @@ int main(int argc, char *argv[])
 			case MUIV_Application_ReturnID_Quit:
 				if ((MUI_RequestA(App->App, 0, 0, "Quit?", "_Yes|_No", "\33cAre you sure?", 0)) == 1)
 					running = FALSE;
-				break;
-
-			// Simple Arexx-Message (without Parameters)
-			case ID_ARexx:
-				DisplayBeep(NULL);
-				printf("A simple ARexx-Command \"test\" received\n");
 				break;
 
 			default:
@@ -310,14 +127,6 @@ void init(void)
 		CloseLibrary((struct Library *)IntuitionBase);
 		exit(20);
 	}
-
-	if (!(CxBase = OpenLibrary("commodities.library", 37)))
-	{
-		printf("Can't Open Commodities Library\n");
-		CloseLibrary((struct Library *)MUIMasterBase);
-		CloseLibrary((struct Library *)IntuitionBase);
-		exit(20);
-	}
 }
 
 /*-----------------------------------------------------------------------------
@@ -325,7 +134,6 @@ void init(void)
 ------------------------------------------------------------------------------*/
 void end(void)
 {
-	CloseLibrary((struct Library *)CxBase);
 	CloseLibrary((struct Library *)MUIMasterBase);
 	CloseLibrary((struct Library *)IntuitionBase);
 	exit(20);
@@ -338,164 +146,85 @@ struct ObjApp *CreateApp(void)
 {
 	struct ObjApp * ObjectApp;
 
-	APTR MNlabel0Project, MNlabel0New, MNlabel0BarLabel0, MNlabel0Load, MNlabel0Save;
-	APTR MNlabel0SaveAa, MNlabel0Transfer, MNlabel0BarLabel1, MNlabel0About;
-	APTR MNlabel0BarLabel2, MNlabel0Hide, MNlabel0Quit, MNlabel0Item, MNlabel0Init;
-	APTR MNlabel0Copy, MNlabel0Paste, MNlabel0BarLabel3, MNlabel0ClearScreen;
-	APTR MNlabel0On, MNlabel0Off;
+	APTR	GROUP_ROOT_0, GR_grp_1, LA_label_3, obj_aux0, obj_aux1, obj_aux2;
+	APTR	obj_aux3;
 
-	APTR GROUP_ROOT_0;
-	
 	if (!(ObjectApp = AllocVec(sizeof(struct ObjApp),MEMF_CLEAR)))
 		return(NULL);
 
-	ObjectApp->STR_TX_label_0 = "This is just a Test for the MUI-ARexx/Commodity-Functionality";
+	ObjectApp->STR_TX_Receive = NULL;
 
-	ObjectApp->TX_label_0 = TextObject,
+	LA_label_3 = Label("Received:");
+
+	ObjectApp->TX_Receive = TextObject,
 		MUIA_Background,	MUII_TextBack,
-		MUIA_Frame, 		MUIV_Frame_Text,
-		MUIA_Text_Contents, ObjectApp->STR_TX_label_0,
-		MUIA_Text_SetMin, 	TRUE,
+		MUIA_Frame,			MUIV_Frame_Text,
+		MUIA_Text_Contents,	ObjectApp->STR_TX_Receive,
 	End;
 
-	ObjectApp->BT_label_0 = SimpleButton("Button 0");
-	ObjectApp->BT_label_1 = SimpleButton("Button 1");
+	GR_grp_1 = GroupObject,
+		MUIA_HelpNode,		"GR_grp_1",
+		MUIA_Group_Columns,	2,
+		Child,				LA_label_3,
+		Child,				ObjectApp->TX_Receive,
+	End;
+
+	ObjectApp->STR_Value = StringObject,
+		MUIA_Frame,			MUIV_Frame_String,
+		MUIA_HelpNode,		"STR_Value",
+		MUIA_String_MaxLen,	4,
+	End;
+
+	obj_aux1 = Label2("Value:");
+
+	obj_aux0 = GroupObject,
+		MUIA_Group_Columns,	2,
+		Child,				obj_aux1,
+		Child,				ObjectApp->STR_Value,
+	End;
+
+	ObjectApp->SL_Value2 = SliderObject,
+		MUIA_HelpNode,		"SL_Value2",
+		MUIA_Frame,			MUIV_Frame_Slider,
+		MUIA_Slider_Min,	0,
+		MUIA_Slider_Max,	100,
+		MUIA_Slider_Level,	0,
+	End;
+
+	obj_aux3 = Label2("Value2:");
+
+	obj_aux2 = GroupObject,
+		MUIA_Group_Columns,	2,
+		Child,				obj_aux3,
+		Child,				ObjectApp->SL_Value2,
+	End;
+
+	ObjectApp->BT_Send = SimpleButton("Send ARexx");
 
 	GROUP_ROOT_0 = GroupObject,
-		Child, ObjectApp->TX_label_0,
-		Child, ObjectApp->BT_label_0,
-		Child, ObjectApp->BT_label_1,
+		Child,	GR_grp_1,
+		Child,	obj_aux0,
+		Child,	obj_aux2,
+		Child,	ObjectApp->BT_Send,
 	End;
 
 	ObjectApp->WI_label_0 = WindowObject,
-		MUIA_Window_Title, 		"MUI-ACT",
-		MUIA_Window_ID, 		MAKE_ID('0', 'W', 'I', 'N'),
-		MUIA_Window_SizeGadget, FALSE,
-		WindowContents, 		GROUP_ROOT_0,
-	End;
-
-	MNlabel0New = MenuitemObject,
-		MUIA_Menuitem_Title, "New",
-		MUIA_Menuitem_Shortcut, "N",
-	End;
-
-	MNlabel0BarLabel0 = MUI_MakeObject(MUIO_Menuitem, NM_BARLABEL, 0, 0, 0);
-
-	MNlabel0Load = MenuitemObject,
-		MUIA_Menuitem_Title, "Load...",
-		MUIA_Menuitem_Shortcut, "O",
-	End;
-
-	MNlabel0Save = MenuitemObject,
-		MUIA_Menuitem_Title, "Save",
-		MUIA_Menuitem_Shortcut, "S",
-	End;
-
-	MNlabel0SaveAa = MenuitemObject,
-		MUIA_Menuitem_Title, "Save Aa...",
-		MUIA_Menuitem_Shortcut, "A",
-	End;
-
-	MNlabel0Transfer = MenuitemObject,
-		MUIA_Menuitem_Title, "Transfer",
-	End;
-
-	MNlabel0BarLabel1 = MUI_MakeObject(MUIO_Menuitem, NM_BARLABEL, 0, 0, 0);
-
-	MNlabel0About = MenuitemObject,
-		MUIA_Menuitem_Title, "About...",
-	End;
-
-	MNlabel0BarLabel2 = MUI_MakeObject(MUIO_Menuitem, NM_BARLABEL, 0, 0, 0);
-
-	MNlabel0Hide = MenuitemObject,
-		MUIA_Menuitem_Title, "Hide",
-		MUIA_Menuitem_Shortcut, "L",
-	End;
-
-	MNlabel0Quit = MenuitemObject,
-		MUIA_Menuitem_Title, "Quit",
-		MUIA_Menuitem_Shortcut, "Q",
-	End;
-
-	MNlabel0Project = MenuitemObject,
-		MUIA_Menuitem_Title, "Project",
-		MUIA_Family_Child, MNlabel0New,
-		MUIA_Family_Child, MNlabel0BarLabel0,
-		MUIA_Family_Child, MNlabel0Load,
-		MUIA_Family_Child, MNlabel0Save,
-		MUIA_Family_Child, MNlabel0SaveAa,
-		MUIA_Family_Child, MNlabel0Transfer,
-		MUIA_Family_Child, MNlabel0BarLabel1,
-		MUIA_Family_Child, MNlabel0About,
-		MUIA_Family_Child, MNlabel0BarLabel2,
-		MUIA_Family_Child, MNlabel0Hide,
-		MUIA_Family_Child, MNlabel0Quit,
-	End;
-
-	MNlabel0Init = MenuitemObject,
-		MUIA_Menuitem_Title, "Init",
-	End;
-
-	MNlabel0Copy = MenuitemObject,
-		MUIA_Menuitem_Title, "Copy",
-		MUIA_Menuitem_Shortcut, "C",
-	End;
-
-	MNlabel0Paste = MenuitemObject,
-		MUIA_Menuitem_Title, "Paste",
-		MUIA_Menuitem_Shortcut, "V",
-	End;
-
-	MNlabel0BarLabel3 = MUI_MakeObject(MUIO_Menuitem, NM_BARLABEL, 0, 0, 0);
-
-	MNlabel0On = MenuitemObject,
-		MUIA_Menuitem_Title, "On",
-		MUIA_Menuitem_Checkit, TRUE,
-		MUIA_Menuitem_Checked, TRUE,
-	End;
-
-	ObjectApp->MN_CS_ON = MNlabel0On;
-
-	MNlabel0Off = MenuitemObject,
-		MUIA_Menuitem_Title, "Off",
-		MUIA_Menuitem_Checkit, TRUE,
-	End;
-
-	ObjectApp->MN_CS_OFF = MNlabel0Off;
-
-	MNlabel0ClearScreen = MenuitemObject,
-		MUIA_Menuitem_Title, "Clear Screen",
-		MUIA_Family_Child, MNlabel0On,
-		MUIA_Family_Child, MNlabel0Off,
-	End;
-
-	MNlabel0Item = MenuitemObject,
-		MUIA_Menuitem_Title, "Item",
-		MUIA_Family_Child, MNlabel0Init,
-		MUIA_Family_Child, MNlabel0Copy,
-		MUIA_Family_Child, MNlabel0Paste,
-		MUIA_Family_Child, MNlabel0BarLabel3,
-		MUIA_Family_Child, MNlabel0ClearScreen,
-	End;
-
-	ObjectApp->MN_label_0 = MenustripObject,
-		MUIA_Family_Child, MNlabel0Project,
-		MUIA_Family_Child, MNlabel0Item,
+		MUIA_Window_Title,		"MUI_ARexx",
+		MUIA_Window_ID,			MAKE_ID('0', 'W', 'I', 'N'),
+		MUIA_Window_SizeGadget,	FALSE,
+		WindowContents,			GROUP_ROOT_0,
 	End;
 
 	ObjectApp->App = ApplicationObject,
 		MUIA_Application_Author,		"M.Volkel",
-		MUIA_Application_Base,			"MUIACT",	// Arexx-Port: "MUIACT.1"
-		MUIA_Application_Title,			"MUI-ACT",
-		MUIA_Application_Version,		"$VER: MUI-ACT V0.1",
+		MUIA_Application_Base,			"MUIAREXX",
+		MUIA_Application_Title,			"MUI_ARexx",
+		MUIA_Application_Version,		"$VER: MUI_ARexx V0.1",
 		MUIA_Application_Copyright,		"(C)2022 M.Volkel",
-		MUIA_Application_Description,	"Testing MUI-ARexx/Commodity-Functions",
-		MUIA_Application_Menustrip,		ObjectApp->MN_label_0,
-		MUIA_Application_BrokerHook,	&hook_hotkey,
-		MUIA_Application_Commands,		&rexxCommands,
+		MUIA_Application_Description,	"ARexx-Test",
 		SubWindow,						ObjectApp->WI_label_0,
 	End;
+
 
 	if (!ObjectApp->App)
 	{
@@ -506,17 +235,7 @@ struct ObjApp *CreateApp(void)
 	// Window-Close-Method
 	DoMethod(ObjectApp->WI_label_0, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, ObjectApp->App, 2, MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
 
-	// Hook-Methods for Buttons
-	DoMethod(ObjectApp->BT_label_0, MUIM_Notify, MUIA_Pressed, FALSE, MUIV_Notify_Self, 3, MUIM_CallHook, &hook_button, ID_BT0);
-	DoMethod(ObjectApp->BT_label_1, MUIM_Notify, MUIA_Pressed, FALSE, MUIV_Notify_Self, 3, MUIM_CallHook, &hook_button, ID_BT1);
-
-	// Hook-Methods for Menu
-	DoMethod(MNlabel0New, MUIM_Notify, MUIA_Menuitem_Trigger, MUIV_EveryTime, ObjectApp->App, 3, MUIM_CallHook, &hook_menu, ID_MN_NEW);
-	DoMethod(MNlabel0Load, MUIM_Notify, MUIA_Menuitem_Trigger, MUIV_EveryTime, ObjectApp->App, 3, MUIM_CallHook, &hook_menu, ID_MN_LOAD);
-	DoMethod(MNlabel0On, MUIM_Notify, MUIA_Menuitem_Trigger, MUIV_EveryTime, ObjectApp->App, 3, MUIM_CallHook, &hook_menu, ID_MN_CSON);
-	DoMethod(MNlabel0Off, MUIM_Notify, MUIA_Menuitem_Trigger, MUIV_EveryTime, ObjectApp->App, 3, MUIM_CallHook, &hook_menu, ID_MN_CSOFF);
-
-	// Window open
+=	// Window open
 	set(ObjectApp->WI_label_0, MUIA_Window_Open, TRUE);
 
 	return(ObjectApp);
